072ae087dfdfa3fb0312066c8a71d46e975726d3
Pedro Fontes <pedro7fontes@gmail.com>
2024-12-23 23:05:46 -0300

    updates init.sh

---
+++ src/database/connection.js
@@ -0,0 +1,23 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+const logger = require("../utils/logger");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+});
+
+// Testar conexão
+pool.on('error', (err) => {
+  logger.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+pool.on('connect', () => {
+  logger.info('Connected to PostgreSQL');
+});
+
+module.exports = { pool }; 
